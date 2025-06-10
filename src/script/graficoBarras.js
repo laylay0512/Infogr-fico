1ec02b873a98dc69f06dc3a45b0a72632845301c
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2025-06-10 13:33:20 -0300

    grafico de barras adicionado

---
+++ src/script/graficoBarras.js
@@ -0,0 +1,43 @@
+const ctx = document.getElementById('graficoBarras').getContext('2d');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+    labels: ['0 a 4', '5 a 9', '10 a 13', '14 a 17', '18 a 19', '20 a 24', '25 a 29', '30 a 34', '35 a 39', '40 a 44', '45 a 49', '50 a 59', '60 ou +'],
+    datasets: [{
+        label: 'Percentual (%)',
+        data: [8.3, 19.0, 32.5, 16.0, 2.9, 5.1, 3.0, 2.9, 2.4, 1.3, 0.8, 0.5, 0.3],
+        backgroundColor: '#5E2A7B'
+    }]
+    },
+    options: {
+    responsive: true,
+    scales: {
+        y: {
+        beginAtZero: true,
+        title: {
+            display: true,
+            text: 'Percentual (%)'
+        }
+        },
+        x: {
+        title: {
+            display: true,
+            text: 'Faixa Etária'
+        }
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        },
+        tooltip: {
+        callbacks: {
+            label: function(context) {
+            return context.raw + '%';
+            }
+        }
+        }
+    }
+    }
+});
